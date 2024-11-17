465d1fc1958dbcbd6cda8d99daff4f57ff2efcd8
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2024-11-17 00:31:28 -0500

    init

---
+++ build-logic/src/main/kotlin/buildlogic.scala-library-conventions.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    id("buildlogic.scala-common-conventions")
+    `java-library`
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+  includeBuild("build-logic")
+}
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+dependencyResolutionManagement {
+  repositories {
+    mavenCentral()
+  }
+}
+
+rootProject.name = "ScaLaMatrixRain"
+
+include(
+  "app",
+)
