e96e3a9385b52a3b2e9f97d6e0145f3d6702a92d
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2025-08-17 01:01:32 -0400

    gradle wrapper --gradle-version latest
    ./gradlew dependenciesResolveAndLockAll shadowJar --write-locks
    
    settings.gradle.kts: dependencyLocking: ignoredDependencies: foojay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,14 @@ pluginManagement {
 buildscript {
   dependencyLocking {
     lockAllConfigurations()
+    //these are expected to show up in settings-gradle.lockfile, but for some unknown reason it is empty
+    ignoredDependencies.add("org.gradle.toolchains.foojay-resolver-convention:*")
+    ignoredDependencies.add("org.gradle.toolchains:foojay-resolver")
   }
 }
 
 plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+  id("org.gradle.toolchains.foojay-resolver-convention") version "latest.release"
 }
 
 dependencyResolutionManagement {
