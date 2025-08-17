880e8bbe1c07ff02718d6fcc38adb521bd6c3a99
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2025-08-17 01:57:28 -0400

    plugins: id("com.gradleup.shadow") version "9.+"

--- slmatrix/build.gradle.kts
+++ slmatrix/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
 plugins {
   id("buildlogic.scala-application-conventions")
-  id("com.gradleup.shadow") version "8.3.5"
+  id("com.gradleup.shadow") version "9.+"
   id("org.graalvm.buildtools.native") version "0.10.3"
 }
 
