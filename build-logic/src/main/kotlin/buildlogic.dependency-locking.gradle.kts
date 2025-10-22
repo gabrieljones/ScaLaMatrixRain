21483bfc5ad795f4e726efa90173e05a254015b9
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2025-10-22 00:12:35 -0400

    dependency-locking: resolve configuration cache issues

--- build-logic/src/main/kotlin/buildlogic.dependency-locking.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.dependency-locking.gradle.kts
@@ -1,21 +1,20 @@
 tasks.register("dependenciesResolve") {
-  notCompatibleWithConfigurationCache("Filters configurations at execution time")
   description = "will fail if `depdendencies --write-locks` did not work as expected. `dependencies --write-locks` will result in successful even if some dependencies failed to resolve"
+  val resolvableConfigurations = configurations.filter { it.isCanBeResolved }
   doLast {
-    configurations.filter { it.isCanBeResolved }
-      .forEach { it.resolve() }
+    resolvableConfigurations.forEach { it.resolve() }
   }
 }
 
 //adapted from https://docs.gradle.org/current/userguide/dependency_locking.html#lock_all_configurations_in_one_build_execution
 tasks.register("dependenciesResolveAndLockAll") {
-  notCompatibleWithConfigurationCache("Filters configurations at execution time")
+  val resolvableConfigurations = configurations.filter { it.isCanBeResolved }
+  val isWriteDependencyLocks = gradle.startParameter.isWriteDependencyLocks
   doFirst {
-    require(gradle.startParameter.isWriteDependencyLocks) { "$path must be run from the command line with the `--write-locks` flag" }
+    require(isWriteDependencyLocks) { "$path must be run from the command line with the `--write-locks` flag" }
   }
   doLast {
-    configurations.filter { it.isCanBeResolved }
-      .forEach { it.resolve() }
+    resolvableConfigurations.forEach { it.resolve() }
   }
 }
 
